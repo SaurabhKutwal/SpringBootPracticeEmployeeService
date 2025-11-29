826513eb14cba326b85acec5140aca00a98780df
saurabh <kutwalsaurabh19@gmail.com>
2025-11-29 19:30:35 +0530

    Entity added

---
+++ src/main/java/com/elite/springboot/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.elite.springboot.employeeapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    //Get All Employees
+
+    //Get Employee by Id
+
+    //Add Employee
+
+    //Update Employee Detail
+
+    //Delete an Employee
+}
